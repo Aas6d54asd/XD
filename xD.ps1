b4831067c977bf199658512429962355d53555fc
Aas6d54asd <editorakmrdragos@gmail.com>
2026-05-29 22:07:18 +0200

    Update xD.ps1

--- xD.ps1
+++ xD.ps1
@@ -1,7 +1,7 @@
 ﻿# ================== NASTAVENÍ ==================
 $GifUrl = "https://media.tenor.com/T-GJmpztYA4AAAAM/son-folk.gif"
 
-$PocetOken = 500
+$PocetOken = 65
 $Sirka = 235
 $Vyska = 270
 $Rychlost = 1
@@ -41,7 +41,7 @@ for ($i = 1; $i -le $PocetOken; $i++) {
     $directionY = if ((Get-Random -Minimum 0 -Maximum 2) -eq 0) { -$Rychlost } else { $Rychlost }
 
     $timer = New-Object System.Windows.Forms.Timer
-    $timer.Interval = 25
+    $timer.Interval = 15
 
     $timer.Add_Tick({
         $newX = $form.Left + $directionX
