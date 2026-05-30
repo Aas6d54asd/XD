378ef3003b726f953f2839dea4b48c5bb08aeaba
Aas6d54asd <editorakmrdragos@gmail.com>
2026-05-30 13:57:19 +0200

    Increase window count and adjust timer interval

--- xD.ps1
+++ xD.ps1
@@ -1,7 +1,7 @@
 ﻿# ================== NASTAVENÍ ==================
 $GifUrl = "https://media.tenor.com/T-GJmpztYA4AAAAM/son-folk.gif"
 
-$PocetOken = 10
+$PocetOken = 80
 $Sirka = 235
 $Vyska = 270
 $Rychlost = 1
@@ -41,7 +41,7 @@ for ($i = 1; $i -le $PocetOken; $i++) {
     $directionY = if ((Get-Random -Minimum 0 -Maximum 2) -eq 0) { -$Rychlost } else { $Rychlost }
 
     $timer = New-Object System.Windows.Forms.Timer
-    $timer.Interval = 300
+    $timer.Interval = 1
 
     $timer.Add_Tick({
         $newX = $form.Left + $directionX
